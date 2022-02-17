6485d744787955c832e728a600b6d37f200180eb
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-17 10:02:44 -0500

    Pastille du bouton compte le nombre de clics

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,20 @@ function App() {
 
   const etatPanier = useState({});
   const panier = etatPanier[0];
-  const setPanier = etatPanier[1];
+  //const setPanier = etatPanier[1];
 
   console.log("L'état panier: ", panier);
+
+  const[compteur, setCompteur] = useState(0);
 
   return (
     <div className="App">
       <Entete panier={panier}/>
-      <ListeProduits panier={panier} setPanier={setPanier}/>
+      <ListeProduits etatPanier={etatPanier}/>
+      <div>
+        <span>Nombres de clicks : {compteur}</span>
+        <button onClick={() => setCompteur(compteur+1)}>cliquez moi</button>
+      </div>
       <PiedPage />
     </div>
   );
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -3,20 +3,29 @@ import BoutonAjoutPanier from './BoutonAjoutPanier';
 
 export default function Produit(props) {
     //Si l'article est dans le panier, afficher une pastille avec sa qte, sinon elle est 0 et rien ne s'affiche
-    let panier = props.panier;
-    let setPanier = props.setPanier;
+    const [panier, setPanier] = props.etatPanier;
+
     let qte = 0;
-    if(props.panier[props.pid]){
-        qte = props.panier[props.pid].qte;
+    if(panier[props.pid]){
+        qte = panier[props.pid].qte;
     }
 
     function ajouterAuPanier(){
-        panier[props.pid] = {
-            prix: props.prix,
-            qte: 1
-        };
-        console.log("Panier: ", panier);
-        //Notifier React que le panier à changé
+
+        if(panier[props.pid]){
+            panier[props.pid].qte++;
+        }
+        else {
+            panier[props.pid] = {
+                prix: props.prix,
+                qte: 1
+            };
+        }
+        console.log("le panier après ajout: ", panier);
+        //Notifier React que le panier à changé:
+        //Il faut cloner l'objet panier pour que React détecte que le panier a changé
+        //setPanier(JSON.parse(JSON.stringify(panier))); Cette méthode clone le panier en temps que string, puis la retransforme en objet
+        //setPanier(Object.assign({}, panier)); Cette méthode fait... quelque chose.
         setPanier({...panier});
     }
 
