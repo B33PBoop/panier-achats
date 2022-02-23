55204358900c849b2e64457ba1f632d50a6b2117
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-23 13:10:06 -0500

    Optimisation du code par déstructuration des props

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -2,13 +2,13 @@ import './Entete.scss';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Badge from '@mui/material/Badge';
 
-export default function Entete(props) {
+export default function Entete({panier}) {
     return (
         <header className='Entete'>
             <h1>Magasin général</h1>
             <nav>
-                <Badge badgeContent={Object.values(props.panier).length} color='primary'>
-                <ShoppingCartIcon />
+                <Badge badgeContent={Object.values(panier).length} color='primary'>
+                    <ShoppingCartIcon />
                 </Badge>
                 <a href="#">Contactez-nous</a>
             </nav>
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,23 +1,21 @@
 import './Produit.scss';
 import BoutonAjoutPanier from './BoutonAjoutPanier';
 
-export default function Produit(props) {
+export default function Produit({etatPanier : [panier, setPanier], nom, prix, pid}) {
     //Si l'article est dans le panier, afficher une pastille avec sa qte, sinon elle est 0 et rien ne s'affiche
-    const [panier, setPanier] = props.etatPanier;
-
     let qte = 0;
-    if(panier[props.pid]){
-        qte = panier[props.pid].qte;
+    if(panier[pid]){
+        qte = panier[pid].qte;
     }
 
     function ajouterAuPanier(){
 
-        if(panier[props.pid]){
-            panier[props.pid].qte++;
+        if(panier[pid]){
+            panier[pid].qte++;
         }
         else {
-            panier[props.pid] = {
-                prix: props.prix,
+            panier[pid] = {
+                prix: prix,
                 qte: 1
             };
         }
@@ -26,14 +24,15 @@ export default function Produit(props) {
         //Il faut cloner l'objet panier pour que React détecte que le panier a changé
         //setPanier(JSON.parse(JSON.stringify(panier))); Cette méthode clone le panier en temps que string, puis la retransforme en objet
         //setPanier(Object.assign({}, panier)); Cette méthode fait... quelque chose.
+        //La méthode ci-dessous utilise le Spread Operator (look it up)
         setPanier({...panier});
     }
 
     return(
         <article className='Produit'>
-            <img src={"images-produits/" + props.pid + ".webp"} alt= {props.nom} />
-            <div className="titre">{props.nom}</div>
-            <div className="prix">{props.prix}</div>
+            <img src={"images-produits/" + pid + ".webp"} alt= {nom} />
+            <div className="titre">{nom}</div>
+            <div className="prix">{prix}</div>
             <BoutonAjoutPanier qte={qte} onClick={ajouterAuPanier}/>                    
         </article>
     )
