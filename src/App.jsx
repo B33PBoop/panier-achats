ef95eb731a450d26eccb831c0fd70fe5ebc15aa5
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-24 09:58:52 -0500

    sommaire du panier et sauvegarde dans localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import Entete from './Entete';
 import PiedPage from './PiedPage';
 import ListeProduits from './ListeProduits';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  //si le panier est déja sauvegardé en localStorage, l'afficher, sinon ouvrir un panier vide
+  const etatPanier = useState(() => JSON.parse(window.localStorage.getItem('panier-4pa')) || {});
 
-  const etatPanier = useState({});
   const panier = etatPanier[0];
   //const setPanier = etatPanier[1];
 
   console.log("L'état panier: ", panier);
 
   const[compteur, setCompteur] = useState(0);
+
+  // sauvegarder le panier dans LocalStorage
+  // utiliser le hook useEffect pour executer ce code de facon controlée
+
+  useEffect(() => window.localStorage.setItem('panier-4pa', JSON.stringify(panier)), [panier]);
 
   return (
     <div className="App">
--- src/Entete.jsx
+++ src/Entete.jsx
@@ -3,15 +3,48 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Badge from '@mui/material/Badge';
 
 export default function Entete({panier}) {
+
+    //obtenire les 5 infos importantes pour le sommaire
+    const {articlesDifferents, articlesTotaux, sousTotal, taxes, total} = calculerInfoPanier(Object.values(panier));
+
     return (
         <header className='Entete'>
             <h1>Magasin général</h1>
             <nav>
-                <Badge badgeContent={Object.values(panier).reduce((acc, article) => acc+article.qte, 0)} color='primary'>
-                    <ShoppingCartIcon />
+                <input type="checkbox" id='cc-sommaire-panier'/>
+                <div className="sommaire-panier">
+                    <h3>Sommaire du panier</h3>
+                    <div><span>Articles Différents</span><span>{articlesDifferents}</span></div>
+                    <div><span>Articles Totaux</span>{articlesTotaux}</div>
+                    <div><span>Sous-total</span><span>{sousTotal}</span></div>
+                    <div><span>Taxes</span><span></span>{taxes}</div>
+                    <div><span>Total</span><span></span>{total}</div>
+                </div>
+
+                <Badge badgeContent={articlesTotaux} color='primary'>
+                    <label htmlFor="cc-sommaire-panier"><ShoppingCartIcon /></label>
                 </Badge>
                 <a href="#">Contactez-nous</a>
             </nav>
         </header>
     );
+}
+
+/** 
+* Calculer l'info du sommaire
+* @param Array panierAchats Objet panier d'achat
+* @returns Object Objet contenant les 5 info requises du panier
+*/
+
+function calculerInfoPanier(panierAchats){
+    const _sousTotal = panierAchats.reduce((acc, courant) => acc+courant.qte*courant.prix, 0);
+    const _taxes = _sousTotal * 0.14975;
+
+    return{
+        articlesDifferents: panierAchats.length,
+        articlesTotaux: panierAchats.reduce((acc, courant) => acc + courant.qte, 0),
+        sousTotal: _sousTotal.toFixed(2),
+        taxes: _taxes.toFixed(2),
+        total: (_sousTotal + _taxes).toFixed(2)
+    }
 }
